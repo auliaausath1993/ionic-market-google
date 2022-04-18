abe285efcbbb37a76e90d04942d6b3d448bc650d
auliaausath1993 <ausathaulia@gmail.com>
2022-04-18 14:44:59 +0700

    redirect update
    redirect update

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -19,14 +19,14 @@ import { StorageMap } from '@ngx-pwa/local-storage';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
+  
   signForm: FormGroup;
   fullName    :string;
   email       :string;
   phoneNumber :number;
   password    :string;
   loading     :any;
-
+  
   constructor(
     private firebaseX: FirebaseX,
     private loadingctrl: LoadingController,
@@ -39,42 +39,42 @@ export class SignupPage implements OnInit {
     private modalCtrl: ModalController,
     private googlePlus: GooglePlus,
     private fireAuth: AngularFireAuth
-  ) { }
-
-  ngOnInit() {
-    this.signForm = this.formBuilder.group({
-      fullName: [null, [Validators.required]],
-      email: [null, [Validators.required]],
-      phoneNumber: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-    });
-  }
-  
-  async presentModal() {
-    const modal = await this.modalCtrl.create({
-      component: ModalOtpPage
-    });
-    await modal.present();
-  }
-
-  signUp() {
-    if (this.signForm.valid) {
-      this.fullName    = this.signForm.value.fullName;
-      this.email       = this.signForm.value.email;
-      this.phoneNumber = this.signForm.value.phoneNumber;
-      this.password    = this.signForm.value.password;
-      
-      let postData = {
-        "nama": this.fullName,
-        "email": this.email,
-        "password": this.password,
-        "phone": this.phoneNumber
-      }
-
-      
-      this.presentLoadingWithOptions();
-      setTimeout(() => {
-        this.http.post(APIURL.apiURL + 'register_v2', postData)
+    ) { }
+    
+    ngOnInit() {
+      this.signForm = this.formBuilder.group({
+        fullName: [null, [Validators.required]],
+        email: [null, [Validators.required]],
+        phoneNumber: [null, [Validators.required]],
+        password: [null, [Validators.required]],
+      });
+    }
+    
+    async presentModal() {
+      const modal = await this.modalCtrl.create({
+        component: ModalOtpPage
+      });
+      await modal.present();
+    }
+    
+    signUp() {
+      if (this.signForm.valid) {
+        this.fullName    = this.signForm.value.fullName;
+        this.email       = this.signForm.value.email;
+        this.phoneNumber = this.signForm.value.phoneNumber;
+        this.password    = this.signForm.value.password;
+        
+        let postData = {
+          "nama": this.fullName,
+          "email": this.email,
+          "password": this.password,
+          "phone": this.phoneNumber
+        }
+        
+        
+        this.presentLoadingWithOptions();
+        setTimeout(() => {
+          this.http.post(APIURL.apiURL + 'register_v2', postData)
           .subscribe(data => {
             console.log(data);
             let status = data['status'];
@@ -110,108 +110,93 @@ export class SignupPage implements OnInit {
               });
             }
           });
+          
+        }, 1500);
+      } 
+      else{
         
-      }, 1500);
-    } 
-    else{
-      
-    }
-  }
-
-  signInWithGoogle(){
-    let params: any;
-    if (this.platform.is('cordova')) {
-      if (this.platform.is('android')) {
-        params = {
-          webClientId: '851911116686-tm6heij2ltrdubqtjo4645mi79nh3vid.apps.googleusercontent.com', //  webclientID 'string'
-          offline: true
-        };
-      } else {
-        params = {
-          webClientId: '851911116686-tm6heij2ltrdubqtjo4645mi79nh3vid.apps.googleusercontent.com', //  webclientID 'string'
-          offline: true
-        };
       }
-      this.googlePlus.login(params)
-      .then((response) => {
-        const { idToken, accessToken } = response;
-        console.log(JSON.stringify(response));
-        this.onLoginSuccess(idToken, accessToken);
-      }).catch((error) => {
-        console.log(error);
-        console.log('error:' + JSON.stringify(error));
-      });
-    } else{
-      console.log('else...');
-      this.fireAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider()).then(success => {
-        console.log('success in google login', success);
+    }
+    
+    signInWithGoogle(){
+      let params: any;
+      if (this.platform.is('cordova')) {
+        if (this.platform.is('android')) {
+          params = {
+            webClientId: '851911116686-tm6heij2ltrdubqtjo4645mi79nh3vid.apps.googleusercontent.com', //  webclientID 'string'
+            offline: true
+          };
+        } else {
+          params = {
+            webClientId: '851911116686-tm6heij2ltrdubqtjo4645mi79nh3vid.apps.googleusercontent.com', //  webclientID 'string'
+            offline: true
+          };
+        }
+        this.googlePlus.login(params)
+        .then((response) => {
+          const { idToken, accessToken } = response;
+          console.log(JSON.stringify(response));
+          this.onLoginSuccess(idToken, accessToken);
+        }).catch((error) => {
+          console.log(error);
+          console.log('error:' + JSON.stringify(error));
+        });
+      } else{
+        console.log('else...');
+        this.fireAuth.signInWithPopup(new firebase.default.auth.GoogleAuthProvider()).then(success => {
+          console.log('success in google login', success);
+          this.doRegister(success.user);
+        }).catch(err => {
+          console.log(err.message, 'error in google login');
+        });
+      }
+    }
+    
+    onLoginSuccess(accessToken, accessSecret) {
+      const credential = accessSecret ? firebase.default.auth.GoogleAuthProvider
+      .credential(accessToken, accessSecret) : firebase.default.auth.GoogleAuthProvider
+      .credential(accessToken);
+      this.fireAuth.signInWithCredential(credential)
+      .then((success) => {
         this.doRegister(success.user);
-      }).catch(err => {
-        console.log(err.message, 'error in google login');
+        this.loading.dismiss();
+      }).catch(error => {
+        console.log('l ' + JSON.stringify(error))
       });
     }
-  }
-
-  onLoginSuccess(accessToken, accessSecret) {
-    const credential = accessSecret ? firebase.default.auth.GoogleAuthProvider
-        .credential(accessToken, accessSecret) : firebase.default.auth.GoogleAuthProvider
-            .credential(accessToken);
-    this.fireAuth.signInWithCredential(credential)
-    .then((success) => {
-      this.doRegister(success.user);
-      this.loading.dismiss();
-    }).catch(error => {
-      console.log('l ' + JSON.stringify(error))
-    });
-  }
-
-  doRegister(data){
-    let val = data;
-    console.log(val);
-    this.loading = this.loadingctrl.create({
-      message: 'Please Wait'
-    }).then((res) => {
-      res.present();
-
-      res.onDidDismiss().then((dis) => {
-        //console.log('Loading dismissed!');
+    
+    doRegister(data){
+      let val = data;
+      console.log(val);
+      this.loading = this.loadingctrl.create({
+        message: 'Please Wait'
+      }).then((res) => {
+        res.present();
+        
+        res.onDidDismiss().then((dis) => {
+          //console.log('Loading dismissed!');
+        });
       });
-    });
-
-    let postData = {
-      "nama": val.name,
-      "googleId": val.googleId,
-      "idToken": val.idToken,
-      "authToken": val.authToken,
-      "email": val.email,
-      "fcm_token": 123
-    }
-
-    this.http.post(APIURL.apiURL + 'register_gmail', postData)
-    .subscribe(data => {
+      
+      let postData = {
+        "nama": val.name,
+        "googleId": val.googleId,
+        "idToken": val.idToken,
+        "authToken": val.authToken,
+        "email": val.email,
+        "fcm_token": 123
+      }
+      
+      this.http.post(APIURL.apiURL + 'register_gmail', postData)
+      .subscribe(data => {
         console.log(data);
         let status = data['status'];
-        /* if(status == "Success"){
+         
+        if(status == "Success"){
           localStorage.setItem('customer_id', data['customer_id']);
           localStorage.setItem('customer_name', data['customer_name']);
           localStorage.setItem('customer_email', data['customer_email']);
           this.router.navigate(['/tabs/shop']);
-        } */
-        if(status == "Success"){
-          this.toastController.create({
-            message: data['message'],
-            position: 'bottom',
-            duration: 5000,
-            buttons: [
-              {
-                side: 'end',
-                icon: 'information-circle'
-              }
-            ]
-          }).then((toast) => {
-            toast.present();
-          });
-          this.presentModal();
         }
         else{
           this.toastController.create({
@@ -229,19 +214,20 @@ export class SignupPage implements OnInit {
           });
         }
       });
+    }
+    
+    signIn(){
+      this.router.navigate(['/signin']);
+    }
+    async presentLoadingWithOptions() {
+      const loading = await this.loadingctrl.create({
+        spinner: 'lines',
+        duration: 1500,
+        message: 'Please wait...',
+        translucent: true,
+        cssClass: 'custom-class custom-loading'
+      });
+      return await loading.present();
+    }
   }
-
-  signIn(){
-    this.router.navigate(['/signin']);
-  }
-  async presentLoadingWithOptions() {
-    const loading = await this.loadingctrl.create({
-      spinner: 'lines',
-      duration: 1500,
-      message: 'Please wait...',
-      translucent: true,
-      cssClass: 'custom-class custom-loading'
-    });
-    return await loading.present();
-  }
-}
+  
